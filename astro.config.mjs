c8fdc68a87d6fc6d48d61e0ad3f8629046f3af52
César Enriquez <136645895+CesarS39@users.noreply.github.com>
2025-08-14 20:12:03 -0600

    fix: GithubPages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+const isProd = process.env.NODE_ENV === 'production';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cesars39.github.io',
-  base: '/MiGanado/',
+  site: 'https://CesarS39.github.io',
+  base: isProd ? '/MiGanado/' : '/',
   vite: {
     plugins: [tailwindcss()]
   }
